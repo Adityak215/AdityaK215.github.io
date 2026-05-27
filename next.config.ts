5d4f82a02226e4a9d4a4a79f83ee92cfe4a4ecf3
Aditya <adityakumar.9c@gmail.com>
2026-05-27 06:16:02 +0530

    deploy

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,11 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: 'export',
+  // Next.js Image Optimization requires a server. This disables it so standard <img> tags work.
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
